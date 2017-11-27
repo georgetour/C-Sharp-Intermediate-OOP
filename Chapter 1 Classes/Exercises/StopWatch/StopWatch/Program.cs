6cf8f64d161ab603c737d915c98bfa858f20076c
George Tourtsinakis <george@besmartbesimple.com>
2017-11-27 12:46:49 +0200

    Created the stopwatch for the exercise
    - It shows seconds that have passed but we can change it

---
+++ Chapter 1 Classes/Exercises/StopWatch/StopWatch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var stopwatch = new StopWatch();
+
+            while (true)
+            {
+                var input = Console.ReadLine().ToLower();
+
+                if (input == "start")   
+                {
+                    stopwatch.Start();
+                }
+                else if(input == "stop")
+                {
+                    stopwatch.Stop();
+                    Console.WriteLine(stopwatch.Duration());
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ Chapter 1 Classes/Exercises/StopWatch/StopWatch/StopWatch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime start;
+        private DateTime stop;
+        private TimeSpan duration;
+        private string output;
+        public bool haveStarted { get; private set; }
+
+        public void Start()
+        {  
+            checkIfstarted();
+            haveStarted = true;
+            Console.WriteLine("Timer started write stop to stop it");
+            start = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            haveStarted = false;
+            stop = DateTime.Now;
+        }
+        
+
+
+        public string Duration()
+        { 
+            duration = stop - start;
+            output = String.Format("{0} seconds have passed",duration.Seconds);
+            return output;
+        }
+
+        public void checkIfstarted()
+        {
+            if (haveStarted)
+            {
+                throw new InvalidOperationException("Timer have already been started");
+            }
+
+        }
+
+
+    }
+}
