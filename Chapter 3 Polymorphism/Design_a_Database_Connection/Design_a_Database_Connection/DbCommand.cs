88ccdee71f01c7af7e2576e5752fb66acf43b78d
George Tourtsinakis <george@besmartbesimple.com>
2017-12-09 15:55:37 +0200

    Exercise 2 DbCommand
    - Added DbCommand execution to the db according to which DbConnection

---
+++ Chapter 3 Polymorphism/Design_a_Database_Connection/Design_a_Database_Connection/DbCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_a_Database_Connection
+{
+    public class DbCommand:DbConnection
+    {
+        public DbConnection _newConnection { get; set; }
+        public string _dbCommand { get; set; }
+
+        public DbCommand(DbConnection newConnection,string connectionString,string dbCommand)
+            :base(connectionString)
+        {
+            _newConnection = newConnection;
+            _dbCommand = dbCommand;
+        }
+
+        public override void OpenDbConnection()
+        {
+            
+        }
+
+        public override void CloseDbConnection()
+        {
+
+        }
+        
+        //With the Execute we are saying first to OpenDbConnecion 
+        //to the one we want which is a parameter in
+        //the constructor, example SqlConnection or OracleConnection after we have created them
+        //as new objects, then we pass a parameter which will be the command 
+        //and finally we close the connection to current Database
+        public void Execute()
+        {
+            _newConnection.OpenDbConnection();
+            Console.WriteLine(_dbCommand);
+            _newConnection.CloseDbConnection();
+
+        }
+
+
+
+
+
+    }
+}
--- Chapter 3 Polymorphism/Design_a_Database_Connection/Design_a_Database_Connection/Program.cs
+++ Chapter 3 Polymorphism/Design_a_Database_Connection/Design_a_Database_Connection/Program.cs
@@ -13,18 +13,20 @@ namespace Design_a_Database_Connection
 
             var sqlConnection = new SqlConnection("localhost");
             var oracleConnection = new OracleConnection("localhost");
+            var sqlCommand = new DbCommand(sqlConnection,"localhost","Executed command to sql");
+            var orcaleCommand = new DbCommand(sqlConnection, "localhost", "Executed command to oracle");
 
+            //Just some screen guding for what the use must input
             Console.WriteLine("For opening sql type: Sql connect");
             Console.WriteLine("For closing sql type: Sql close");
             Console.WriteLine("For opening oracle type: Oracle connect");
             Console.WriteLine("For closing sql type: Oracle connect");
+            Console.WriteLine("For executing a command type sql command or oracle command");
 
             //sqlConnection.OpenDbConnection();
             //oracleConnection.OpenDbConnection();
             //sqlConnection.CloseDbConnection();
             //oracleConnection.CloseDbConnection();
-
-
 
             while (true)
             {
@@ -44,6 +46,13 @@ namespace Design_a_Database_Connection
                     case "oracle close":
                         oracleConnection.CloseDbConnection();
                         break;
+                    case "sql command":
+                        sqlCommand.Execute();
+                        break;
+                    case "oarcle command":
+                        orcaleCommand.Execute();
+                        break;
+
 
                     default:
                         return;
